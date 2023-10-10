9cb09df7d089d2ccd86901567e20cf63d6a3ab18
Abhyuday <abhyudaykumarabl@gmail.com>
2023-10-11 02:01:07 +0530

    commiting Algerian forest fire files

---
+++ application.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,jsonify,render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+application=Flask(__name__)
+app=application
+
+##import ridge and standardscaler pickle
+
+ridge_model=pickle.load(open('models/algerian_ridge.pkl','rb'))
+standard_scaler=pickle.load(open('models/algerian_scaler.pkl','rb'))
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+
+        print("inside post")
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+        print("data loaded")
+
+
+        new_scaled_data=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        print([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_scaled_data)
+        print(result[0])
+        return render_template('home.html',results=result[0])
+    else:
+        return render_template('home.html')
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
